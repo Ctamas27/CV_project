365f046042b109e72499ddd0a0950e5d3791d872
Tomi <ctamas27@gmail.com>
2022-07-10 22:02:50 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function aboutMe() {
+    document.getElementById("demo").innerHTML = "This is about me";
+}
+
+function exampleProjects(){
+    document.getElementById("demo").innerHTML = <button type="button" onclick="">Hi</button>;
+}
+
+function cv(){
+    document.getElementById("demo").innerHTML = "This is the cv site";
+} 
+
+//window.alert('Yo');
